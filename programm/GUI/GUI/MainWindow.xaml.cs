cd3338638941f1c8fbba0b50435fd3c5c67f58ea
Azim Izzudin Ramadhani Mubarak <s0571801@htw-berlin.de>
2021-05-29 10:55:03 +0200

    MainWindow.xaml.cs einfügen

--- programm/GUI/GUI/MainWindow.xaml.cs
+++ programm/GUI/GUI/MainWindow.xaml.cs
@@ -23,6 +23,70 @@ namespace GUI
         public MainWindow()
         {
             InitializeComponent();
+
+            Main.Navigate(new Home());
+        }
+
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Set tooltip visibility
+
+            if (Tg_Btn.IsChecked == true)
+            {
+                tt_home.Visibility = Visibility.Collapsed;
+                tt_contact.Visibility = Visibility.Collapsed;
+                tt_how.Visibility = Visibility.Collapsed;
+                tt_info.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                tt_home.Visibility = Visibility.Visible;
+                tt_contact.Visibility = Visibility.Visible;
+                tt_how.Visibility = Visibility.Visible;
+                tt_info.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Tg_Btn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            img_bg.Opacity = 1;
+            Main.Opacity = 1;
+        }
+
+        private void Tg_Btn_Checked(object sender, RoutedEventArgs e)
+        {
+            img_bg.Opacity = 0.3;
+            Main.Opacity = 0.3;
+        }
+
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_Btn.IsChecked = false;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void StackPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Main.Content = new About();
+        }
+
+        private void StackPanel_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+            Main.Content = new Contact();
+        }
+
+        private void StackPanel_MouseLeftButtonDown_2(object sender, MouseButtonEventArgs e)
+        {
+            Main.Content = new Info();
+        }
+
+        private void StackPanel_MouseLeftButtonDown_3(object sender, MouseButtonEventArgs e)
+        {
+            Main.Content = new Home();
         }
     }
 }
