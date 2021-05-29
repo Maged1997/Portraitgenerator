d7660c614025c78fa630488a8f5ace542ed1cb62
Azim Izzudin Ramadhani Mubarak <s0571801@htw-berlin.de>
2021-05-29 11:24:43 +0200

    How Page und CustomControl einfügen

---
+++ programm/GUI/GUI/CustomControl/fileDetail.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI.CustomControl
+{
+    /// <summary>
+    /// Interaction logic for fileDetail.xaml
+    /// </summary>
+    public partial class fileDetail : UserControl
+    {
+        public fileDetail()
+        {
+            InitializeComponent();
+        }
+
+        public string FileName
+        {
+            get { return (string)GetValue(FileNameProperty); }
+            set { SetValue(FileNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty FileNameProperty =
+            DependencyProperty.Register("FileName", typeof(string), typeof(fileDetail));
+
+        public string FileSize
+        {
+            get { return (string)GetValue(FileSizeProperty); }
+            set { SetValue(FileSizeProperty, value); }
+        }
+
+        public static readonly DependencyProperty FileSizeProperty =
+            DependencyProperty.Register("FileSize", typeof(string), typeof(fileDetail));
+
+        public int UploadProgress
+        {
+            get { return (int)GetValue(UploadProgressProperty); }
+            set { SetValue(UploadProgressProperty, value); }
+        }
+
+        public static readonly DependencyProperty UploadProgressProperty =
+            DependencyProperty.Register("UploadProgress", typeof(int), typeof(fileDetail));
+
+        public int UploadSpeed
+        {
+            get { return (int)GetValue(UploadSpeedProperty); }
+            set { SetValue(UploadSpeedProperty, value); }
+        }
+
+        public static readonly DependencyProperty UploadSpeedProperty =
+            DependencyProperty.Register("UploadSpeed", typeof(int), typeof(fileDetail));
+    }
+}
---
+++ programm/GUI/GUI/pageDraft/How.xaml.cs
@@ -0,0 +1,84 @@
+﻿using GUI.CustomControl;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI.pageDraft
+{
+    /// <summary>
+    /// Interaction logic for How.xaml
+    /// </summary>
+    public partial class How : Page
+    {
+        public How()
+        {
+            InitializeComponent();
+        }
+
+        private void Rectangle_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                string fileName = System.IO.Path.GetFileName(files[0]);
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    string filename = System.IO.Path.GetFileName(files[i]);
+                    FileInfo fileInfo = new FileInfo(files[i]);
+                    UploadingFilesList.Items.Add(new fileDetail()
+                    {
+                        FileName = filename,
+
+                        FileSize = string.Format("{0} {1}", (fileInfo.Length / 1.049e+6).ToString("0.0"), "Mb"),
+                        UploadProgress = 100
+                    });
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog() { Multiselect = true };
+            openFileDialog.Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg|All files (*.*)|*.*";
+            bool? response = openFileDialog.ShowDialog();
+
+            if (response == true)
+            {
+                string[] files = openFileDialog.FileNames;
+                Start_Btn.IsEnabled = true;
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    string filename = System.IO.Path.GetFileName(files[i]);
+                    FileInfo fileInfo = new FileInfo(files[i]);
+                    UploadingFilesList.Items.Add(new fileDetail()
+                    {
+                        FileName = filename,
+
+                        FileSize = string.Format("{0} {1}", (fileInfo.Length / 1.049e+6).ToString("0.0"), "Mb"),
+                        UploadProgress = 100
+                    });
+                }
+            }
+        }
+
+        private void Start_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("Image.xaml", UriKind.Relative));
+        }
+    }
+}
